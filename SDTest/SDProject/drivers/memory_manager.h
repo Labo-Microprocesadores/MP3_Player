1c7541dc7074090cc44bba1f9033d0d3534f139d
SantiAA <sarribere@itba.edu.ar>
2021-01-14 21:35:51 -0300

    movimiento entre archivos ready

--- SDTest/SDProject/drivers/memory_manager.h
+++ SDTest/SDProject/drivers/memory_manager.h
@@ -8,6 +8,11 @@
 #ifndef MEMORY_MANAGER_H_
 #define MEMORY_MANAGER_H_
 
+/*******************************************************************************
+ * INCLUDE HEADER FILES
+ ******************************************************************************/
+
+#include <stdbool.h>
 
 /*******************************************************************************
  * ENUMERATIONS AND STRUCTURES AND TYPEDEFS
@@ -20,6 +25,19 @@
 
 void Mm_Init(void);
 
+char * Mm_GetCurrentPath(void);
+char * Mm_GetCurrentName(void);
 
+void Mm_GoNext(void);
+bool Mm_IsNext(void);
+
+void Mm_GoPrev(void);
+bool Mm_IsPrev(void);
+
+void Mm_GoInDir(void);
+void Mm_GoOutDir(void);
+
+bool Mm_CanGoInDir(void);
+bool Mm_CanGoOutDir(void);
 
 #endif /* MEMORY_MANAGER_H_ */
