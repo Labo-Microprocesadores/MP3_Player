15dca750337559c5b4026bdd357d953d8541f94b
SantiAA <sarribere@itba.edu.ar>
2021-01-15 21:12:26 -0300

    proyecto de math

--- SDTest/SDProject/Debug/component/fatfs/ffunicode.d
+++
@@ -1,7 +0,0 @@
-component/fatfs/ffunicode.o component/fatfs/ffunicode.d: \
- ../component/fatfs/ffunicode.c ../component/fatfs/ff.h \
- ../component/fatfs/ffconf.h
-
-../component/fatfs/ff.h:
-
-../component/fatfs/ffconf.h:
--- SDTest/SDProject/Debug/drivers/file_system_manager.d
+++
@@ -1,4 +0,0 @@
-drivers/file_system_manager.o drivers/file_system_manager.d: \
- ../drivers/file_system_manager.c ../drivers/file_system_manager.h
-
-../drivers/file_system_manager.h:
