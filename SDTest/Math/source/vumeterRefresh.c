68da26318fcd9f2ef510c39326ca098f05276bde
carmaselli <37149316+carmaselli@users.noreply.github.com>
2021-01-18 14:47:35 -0300

    terminado sin testear

--- SDTest/Math/source/vumeterRefresh.c
+++ SDTest/Math/source/vumeterRefresh.c
@@ -4,25 +4,30 @@
 #define SAMPLE_LENGTH       1024
 #define NUMBER_OF_BANDS     8  
 #define VUMETER_HEIGHT      8
+#define NOISE               200
+#define MAX_AMPLITUDE       1000
 
 
+static  arm_rfft_fast_instance_f32 rfft_fast_instance;
 
-// poner en algun lado: arm_rfft_fast_init_f32(&rfft_fast_instance, fftLen);
+
 void vumeterRefresh_init()
 {
-    
+    arm_rfft_fast_init_f32(&rfft_fast_instance, SAMPLE_LENGTH);
 }
+
+
 int vumeterRefresh_fft(float32_t * inputSignal, float32_t sampleRate, float32_t *samples, float32_t lowerFreqBand, float32_t higherFreqBand)
 { 
     float32_t vumeterValues[NUMBER_OF_BANDS];
-    int vumeterMatrix[VUMETER_HEIGHT][NUMBER_OF_BANDS];
+    int vumeterMatrix[NUMBER_OF_BANDS];
     float32_t nyquistFreq = sampleRate / 2;
     double binWidth = sampleRate / SAMPLE_LENGTH;
     unsigned int usableBins = roof(SAMPLE_LENGTH / 2 - 1);
     float32_t realHigherFreqBand = higherFreqBand  < nyquistFreq * 0.99 ? higherFreqBand :  nyquistFreq * 0.99; 
     float32_t freqMultiplierPerBand = pow((higherFreqBand / lowerFreqBand), (1 / (NUMBER_OF_BANDS - 1 )));
 
-    arm_rfft_fast_instance_f32 rfft_fast_instance;
+   
     float32_t output[SAMPLE_LENGTH / 2];
     float32_t outputFft[SAMPLE_LENGTH / 2];
     arm_rfft_fast_f32(&rfft_fast_instance, inputSignal, output, 0);
@@ -32,6 +37,7 @@ int vumeterRefresh_fft(float32_t * inputSignal, float32_t sampleRate, float32_t 
 
     //unsigned int binFreq[NUMBER_OF_BANDS];
     float32_t currentBinFreq;
+    float32_t nextBinFreq;
     unsigned int currentCenterBin;
     unsigned int nextCenterBin;
     unsigned int lowerBin = 0;
@@ -40,10 +46,19 @@ int vumeterRefresh_fft(float32_t * inputSignal, float32_t sampleRate, float32_t 
     {
         currentBinFreq = lowerFreqBand * pow(freqMultiplierPerBand, i);
         currentCenterBin = currentBinFreq / binWidth;
-        //calcular higherBin!!!
-        if(i!=0)
+        lowerBin = i ? higherBin : 0;
+        higherBin = (nextCenterBin-currentCenterBin)/2 + currentCenterBin;//calcular higherBin!!!
+        
+        for (size_t j = lowerBin; j < higherBin; j++)
         {
-            lowerBin = higherBin;
+           if(outputFft[j] > NOISE)  
+           {
+            vumeterValues[i] += outputFft[j]; 
+           }
         }
+        int roundedHeight = floor(vumeterValues[i]/MAX_AMPLITUDE);
+        vumeterMatrix[i] = roundedHeight > VUMETER_HEIGHT ? VUMETER_HEIGHT : roundedHeight;   
     } 
-}
+    //TODO
+
+    //write_to_matrix(vumeterMatrix)
--- SDTest/Math/source/vumeterRefresh.h
+++ SDTest/Math/source/vumeterRefresh.h
@@ -3,6 +3,7 @@
 #include<stdio.h>
 #include<stdint.h>
 
+void vumeterRefresh_init();
 int vumeterRefresh_fft(int sample_rate, float32_t *samples, unsigned int sampleLength, unsigned int lowerFreqBand,unsigned int higherFreqBand);
 #endif //_VUMETER_REFRESH_H
 
