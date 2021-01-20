e30524b620410541a49aa1ea58366c767da781cb
SantiAA <sarribere@itba.edu.ar>
2021-01-20 16:17:56 -0300

    improving fft funciont

--- fft_test6/source/mainTesting.c
+++ fft_test6/source/mainTesting.c
@@ -3,9 +3,75 @@
 #include "arm_const_structs.h"
 #include "vumeterRefresh.h"
 
+#include "fsl_common.h"
+#include "fsl_port.h"
+#include "fsl_gpio.h"
+#include "clock_config.h"
 
+/***********************************/
+#include <stdint.h>
+#include <stdbool.h>
+#include "fsl_device_registers.h"
+#include "core_cm4.h"
+#include <stdbool.h>
+#include <stdint.h>
+
+#define __CORE_CLOCK__  100000000U
+#define __FOREVER__     for(;;)
+#define __ISR__         void __attribute__ ((interrupt))
+
+static uint32_t __LDM_interruptDisableCount = 0;
+
+void hw_Init (void)
+{
+    SCB->CPACR |= ((3UL << 10*2) | (3UL << 11*2)); /* set CP10, CP11 for Full Access to the FPU*/
+
+    WDOG->UNLOCK  = WDOG_UNLOCK_WDOGUNLOCK(0xC520); /* Key 1 */
+    WDOG->UNLOCK  = WDOG_UNLOCK_WDOGUNLOCK(0xD928); /* Key 2 */
+    WDOG->STCTRLH = WDOG_STCTRLH_ALLOWUPDATE_MASK | WDOG_STCTRLH_CLKSRC_MASK | 0x0100U; /* Disable WDOG */
+
+    PMC->REGSC |= PMC_REGSC_ACKISO_MASK; /* Release hold with ACKISO:  Only has an effect if recovering from VLLSx.*/
+
+    SIM->CLKDIV1 = SIM_CLKDIV1_OUTDIV1(0x00) | SIM_CLKDIV1_OUTDIV2(0x01) | SIM_CLKDIV1_OUTDIV3(0x01) | SIM_CLKDIV1_OUTDIV4(0x03); /* Core-System = 100MHz, Bus = 50MHz, FlexBus = 50MHz, Flash = 25MHz */
+    SIM->SOPT1 |= SIM_SOPT1_OSC32KSEL(0x03); /* Set 32 kHz clock source (ERCLK32K) */
+    SIM->SOPT2 = SIM_SOPT2_PLLFLLSEL_MASK; /* Set high frequency clock source (PLL) */
+    SIM->SCGC5 |= SIM_SCGC5_PORTA_MASK | SIM_SCGC5_PORTB_MASK | SIM_SCGC5_PORTC_MASK | SIM_SCGC5_PORTD_MASK | SIM_SCGC5_PORTE_MASK; /* All PORTs enabled */
+
+    MCG->SC = MCG_SC_FCRDIV(0x02); /* Fast clock internal reference divider */
+    MCG->C2 = MCG_C2_RANGE(0x02); /* High frequency range external reference selection */
+
+    OSC->CR = OSC_CR_ERCLKEN_MASK; /* Set external reference clock (OSCERCLK) */
+
+    MCG->C7 = MCG_C7_OSCSEL(0x00); /* Set FLL external reference clock (OSCCLK0) */
+    MCG->C1 = MCG_C1_CLKS(0x02) | MCG_C1_FRDIV(0x07); /* Set external reference as source, FLL external reference divider (PBE mode) */
+    while((MCG->S & MCG_S_IREFST_MASK) != 0x00U); /* Check external reference validation */
+    MCG->C5 = MCG_C5_PRDIV0(0x0F); /* Set PLL divider while PLL turned off */
+    MCG->C6 = MCG_C6_PLLS_MASK | MCG_C6_VDIV0(0x08); /* Set PLL multiplier and PLL select */
+    while((MCG->S & MCG_S_LOCK0_MASK) == 0x00U); /* Wait until PLL is locked*/
+    MCG->C1 &= ~MCG_C1_CLKS_MASK;
+    while((MCG->S & MCG_S_CLKST_MASK) != 0x0CU); /* Wait until output of the PLL is selected */
+}
+
+/***************************************/
 int main (void)
 {
+	hw_Init();
+	SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK; // activo clock gating
+
+	PORTC->PCR[16] = 0x00;
+	PORTC->PCR[16] |= PORT_PCR_MUX(1);
+	PORTC->PCR[16] |= PORT_PCR_IRQC(0);
+	GPIOC->PDDR |= 1 << 16;
+
+	GPIOC->PDOR &= ~(1 << 16);
+
+	PORTC->PCR[17] = 0x00;
+	PORTC->PCR[17] |= PORT_PCR_MUX(1);
+	PORTC->PCR[17] |= PORT_PCR_IRQC(0);
+	GPIOC->PDDR |= 1 << 17;
+
+	GPIOC->PDOR &= ~(1 << 17);
+
 	vumeterRefresh_init();
 
 	  float32_t baja[1024];
@@ -27,6 +93,17 @@ int main (void)
 	         otra4[i] = 30.0f*arm_sin_f32(2*3.1415926f*15000*i/44100);
 	         signal[i] = baja[i]+otra4[i];
 	     }
+
+	GPIOC->PDOR |= (1 << 16);
 	vumeterRefresh_fft(signal, 44100, 80, 20000);
+	GPIOC->PDOR &= ~(1 << 16);
+
+	GPIOC->PDOR |= (1 << 16);
+	vumeterRefresh_fft(signal, 44100, 80, 20000);
+	GPIOC->PDOR &= ~(1 << 16);
+
+	GPIOC->PDOR |= (1 << 16);
+	vumeterRefresh_fft(signal, 44100, 80, 20000);
+	GPIOC->PDOR &= ~(1 << 16);
 	return 0;
 }
--- fft_test6/source/vumeterRefresh.c
+++ fft_test6/source/vumeterRefresh.c
@@ -2,7 +2,8 @@
 #include "math.h"
 //#include "matrix_display.h"
 #include <stdbool.h>
-
+#include "fsl_device_registers.h"
+#include "vumeterRefresh.h"
 
 typedef struct
 {
@@ -19,67 +20,76 @@ typedef struct
 
 
 static  arm_rfft_fast_instance_f32 rfft_fast_instance;
+static float32_t output[SAMPLE_LENGTH];
+static float32_t outputFft[SAMPLE_LENGTH / 2];
+static int vumeterMatrix[NUMBER_OF_BANDS];
 
+float32_t fpowf(float32_t base, uint16_t n); //Fast exponenciation
 
 void vumeterRefresh_init()
 {
     arm_rfft_fast_init_f32(&rfft_fast_instance, SAMPLE_LENGTH);
 }
 
-float32_t output[SAMPLE_LENGTH];
-float32_t outputFft[SAMPLE_LENGTH / 2];
+
 
 int vumeterRefresh_fft(float32_t * inputSignal, float32_t sampleRate, int lowerFreqBand, int higherFreqBand)
 { 
-    float32_t vumeterValues[NUMBER_OF_BANDS];
-    int vumeterMatrix[NUMBER_OF_BANDS];
+    //float32_t vumeterValues[NUMBER_OF_BANDS];
+    unsigned int usableBins = (SAMPLE_LENGTH / 2 - 1);
+
     float32_t nyquistFreq = sampleRate / 2;
-    double binWidth = sampleRate / SAMPLE_LENGTH;
-    unsigned int usableBins = ceil(SAMPLE_LENGTH / 2 - 1);
-    float32_t realHigherFreqBand = higherFreqBand  < nyquistFreq * 0.99 ? higherFreqBand :  nyquistFreq * 0.99; 
+    double inv_binWidth = SAMPLE_LENGTH/sampleRate;
     double base = higherFreqBand / lowerFreqBand;
-    double exp = 1.0f / (NUMBER_OF_BANDS - 1.0f );
-    double base2 = 20000/80;
-    float32_t base3 = 20000/80;
-    int base4 = 20000/80;
-    float32_t freqMultiplierPerBand = pow(base,exp);
+	double exp = 1.0f / (NUMBER_OF_BANDS - 1.0f );
+	float32_t freqMultiplierPerBand = pow(base,exp);
+
+    //float32_t realHigherFreqBand = higherFreqBand  < nyquistFreq * 0.99 ? higherFreqBand :  nyquistFreq * 0.99;
+    //double base2 = 20000/80;
+    //float32_t base3 = 20000/80;
+    //int base4 = 20000/80;
 
    
-
+    //GPIOC->PDOR |= (1 << 17);
     arm_rfft_fast_f32(&rfft_fast_instance, inputSignal, output, 0);
     /* Process the data through the Complex Magnitude Module for
     calculating the magnitude at each bin */
     arm_cmplx_mag_f32(output, outputFft, SAMPLE_LENGTH);
     outputFft[0] = 0; //Removing DC
+    ///GPIOC->PDOR &= ~(1 << 17);
 
     //unsigned int binFreq[NUMBER_OF_BANDS];
-    float32_t currentBinFreq;
-    float32_t nextBinFreq;
+    float32_t currentBinFreq = lowerFreqBand;
+    float32_t nextBinFreq = lowerFreqBand * freqMultiplierPerBand;
     float32_t currentCenterBin;
     float32_t nextCenterBin;
     float32_t lowerBin = 0;
     float32_t higherBin;
+    float32_t temp;
     for (size_t i = 0; i < NUMBER_OF_BANDS; i++)
     {
-        currentBinFreq = lowerFreqBand * pow(freqMultiplierPerBand, i);
-        nextBinFreq = lowerFreqBand * pow(freqMultiplierPerBand, i+1);
-        currentCenterBin = currentBinFreq / binWidth;
-        nextCenterBin = nextBinFreq / binWidth;
+        //currentBinFreq = lowerFreqBand * fpowf(freqMultiplierPerBand, i);
+        //nextBinFreq = lowerFreqBand * fpowf(freqMultiplierPerBand, i+1);
+
+        currentCenterBin = currentBinFreq * inv_binWidth;
+        nextCenterBin = nextBinFreq * inv_binWidth;
         lowerBin = i ? higherBin : 0;
-        higherBin = (nextCenterBin-currentCenterBin)/2 + currentCenterBin;//calcular higherBin!!!
-        vumeterValues[i] = 0;
-        for (size_t j = floor(lowerBin); ((j < floor(higherBin))&&(j < usableBins)); j++)
+        higherBin = (nextCenterBin-currentCenterBin)*0.5 + currentCenterBin;//calcular higherBin!!!
+
+        temp = 0;
+        for (size_t j = (int)(lowerBin); ((j < (int)(higherBin))&&(j < usableBins)); j++)
         {
-           if(outputFft[j] > NOISE)  
-           {
-            vumeterValues[i] += outputFft[j]; 
-           }
+        	temp += (outputFft[j] > NOISE)? outputFft[j] : 0;
         }
-        int roundedHeight = floor(vumeterValues[i]/MAX_AMPLITUDE);
+        int roundedHeight = (int)(temp/MAX_AMPLITUDE);
         //int roundedHeight = floor((vumeterValues[i]/(higherBin - lowerBin))/1000);
-        vumeterMatrix[i] = roundedHeight > VUMETER_HEIGHT ? VUMETER_HEIGHT : roundedHeight;   
+        vumeterMatrix[i] = roundedHeight > VUMETER_HEIGHT ? VUMETER_HEIGHT : roundedHeight;
+
+        currentBinFreq = nextBinFreq;
+        nextBinFreq *= freqMultiplierPerBand;
     } 
     vumeterRefresh_write_to_matrix(vumeterMatrix);
+    return 0;//???
 }
     //TODO
 
@@ -122,3 +132,18 @@ void vumeterRefresh_write_to_matrix(int * vumeterMatrix)
     //md_writeBuffer(auxMatrix);
 }
 
+
+float32_t fpowf(float32_t base, uint16_t n)
+{
+	float ans = 1;
+	while(n > 0)
+	{
+		if(n%2)
+		{
+			ans *= base;
+		}
+		n = n/2;
+		base *= base;
+	}
+	return ans;
+}
